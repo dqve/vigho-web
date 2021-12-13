2fec38efc8ae75deb84bced8a1e468665c9ad6da
unknown <officialrubee@gmail.com>
2021-12-13 17:54:57 +0100

    section2

--- src/components/AgentComponets/AgentSection2/AgentSection2.js
+++ src/components/AgentComponets/AgentSection2/AgentSection2.js
@@ -1,29 +1,72 @@
 import React from 'react'
 import './AgentSection2.css'
 import { agentSection2Data } from './AgentSection2Data'
+import { Grid } from '@material-ui/core'
 const AgentSection2 = () => {
   return (
-    <div className='ang-sec2-con'>
-      <div className='ang-sec2-text'>
-        <div className='ang-sec2-text-wrap'>
-          <div className='why-you-should-become-a-vigho-agent gtwalsheimpro-bold-licorice-50px-2'>
-            <span className='gtwalsheimpro-bold-licorice-50px'>
-              {agentSection2Data.text1}
-            </span>
-            <span className='gtwalsheimpro-bold-green-haze-50px'>
-              {' '}
-              {agentSection2Data.text2}
-            </span>
-            <span className='gtwalsheimpro-bold-licorice-50px'>
-              {' '}
-              {agentSection2Data.text3}
-            </span>
-          </div>
+    <div className='ang-sec2-con-main'>
+      <div className='ang-sec2-con'>
+        <div className='ang-sec2-text'>
+          <div className='ang-sec2-text-wrap'>
+            <div className='why-you-should-become-a-vigho-agent gtwalsheimpro-bold-licorice-50px-2'>
+              <span className='gtwalsheimpro-bold-licorice-50px'>
+                {agentSection2Data.text1}
+              </span>
+              <span className='gtwalsheimpro-bold-green-haze-50px'>
+                {' '}
+                {agentSection2Data.text2}
+              </span>
+              <span className='gtwalsheimpro-bold-licorice-50px'>
+                {' '}
+                {agentSection2Data.text3}
+              </span>
+            </div>
 
-          <div className='ang-sec2-text4'>{agentSection2Data.text4}</div>
+            <div className='ang-sec2-text4'>{agentSection2Data.text4}</div>
+          </div>
         </div>
+        <div className='empty'></div>
       </div>
-      <div className='empty'></div>
+      <Grid container className='ang-sec2-con'>
+        <Grid item xs={12} sm={6} className='ang-sec2-img'>
+          <img
+            className='ang-sec2-main-img '
+            src={agentSection2Data.phone}
+            alt='phone'
+          />
+        </Grid>
+        <Grid item xs={12} sm={6} className='ang-sec2-text2'>
+          <div className='ang-sec2-col'>
+            <div className='ang-sec2-row'>
+              <div className='ang-min-img'>
+                <img src={agentSection2Data.icon1} alt='icon' />
+              </div>
+              <div className='ang-min-text-con'>
+                <div className='ang-min-text1'>Reliable & Secure Network</div>
+                <div className='ang-min-text2'>{agentSection2Data.text5}</div>
+              </div>
+            </div>
+            <div className='ang-sec2-row'>
+              <div className='ang-min-img'>
+                <img src={agentSection2Data.icon2} alt='icon' />
+              </div>
+              <div className='ang-min-text-con'>
+                <div className='ang-min-text1'>Reliable & Secure Network</div>
+                <div className='ang-min-text2'>{agentSection2Data.text6}</div>
+              </div>
+            </div>
+            <div className='ang-sec2-row'>
+              <div className='ang-min-img'>
+                <img src={agentSection2Data.icon3} alt='icon' />
+              </div>
+              <div className='ang-min-text-con'>
+                <div className='ang-min-text1'>Reliable & Secure Network</div>
+                <div className='ang-min-text2'>{agentSection2Data.text7}</div>
+              </div>
+            </div>
+          </div>
+        </Grid>
+      </Grid>
     </div>
   )
 }
--- src/components/AgentComponets/AgentSection2/AgentSection2Data.js
+++ src/components/AgentComponets/AgentSection2/AgentSection2Data.js
@@ -1,3 +1,7 @@
+import phone from '../../../assets/Phone.svg'
+import icon1 from '../../../assets/sec2-icon1.svg'
+import icon2 from '../../../assets/sec2-icon2.svg'
+import icon3 from '../../../assets/sec2-icon3.svg'
 export const agentSection2Data = {
   text1: (
     <>
@@ -10,6 +14,29 @@ export const agentSection2Data = {
     <>
       Make transfers, withdrawals, and pay bills for your <br />
       customers.
+    </>
+  ),
+  phone: phone,
+  icon1: icon1,
+  icon2: icon2,
+  icon3: icon3,
+  text5: (
+    <>
+      With Vigho, you can be sure not to lose <br /> customers and transactions
+      due to <br /> incessant downtimes and network issues.
+    </>
+  ),
+  text6: (
+    <>
+      We guarantee that all your transactions <br /> are settled instantly, “No
+      Stories”.
+    </>
+  ),
+
+  text7: (
+    <>
+      We make it easy to provide quality access to <br /> financial services in
+      your local communities at <br /> affordable fees
     </>
   ),
 }
